91e6794b554cc1170f0e5eefaaef68f2fcc3f8b2
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-04-17 12:37:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+#! /urs/bin/env  node
+import inquirer from "inquirer";
+let todos = [];
+let condition = 0;
+while (condition <= 5) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you want to add in your Todos",
+            choices: ["Delete", " select ", "item"]
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more",
+            default: "false"
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addmore;
+    console.log(todos);
+    console.log(condition);
+    condition++;
+}
